4c07f744ee18984d25c2f2d2df76176a8342ab71
Mark Stefanovic <markstefanovic@hotmail.com>
2022-09-27 04:57:43 -0700

    start improving logging

--- src/adapter/db.py
+++ src/adapter/db.py
@@ -1,11 +1,51 @@
 import asyncio
+import typing
 
 import asyncpg
 
-__all__ = ("gather_with_limited_concurrency", "get_proc_names_by_pattern")
+__all__ = (
+    "batch_failed",
+    "batch_started",
+    "batch_succeeded",
+    "gather_with_limited_concurrency",
+    "get_proc_names_by_pattern",
+    "proc_failed",
+    "proc_started",
+    "proc_succeeded",
+    "run_proc",
+)
+
+_DEFAULT_TIMEOUT = 10
 
 
-async def gather_with_limited_concurrency(n: int, *tasks):
+async def batch_failed(
+    *,
+    pool: asyncpg.Pool,
+    batch_id: int,
+    error_message: str,
+    context: dict[str, typing.Hashable],
+) -> int:
+    async with pool.acquire(timeout=_DEFAULT_TIMEOUT) as con:
+        return await con.execute(
+            "CALL dwr.batch_failed(p_batch_id := $1, p_error_message := $2, p_context := $3);",
+            batch_id, error_message, context,
+        )
+
+
+async def batch_started(*, pool: asyncpg.Pool) -> int:
+    async with pool.acquire(timeout=_DEFAULT_TIMEOUT) as con:
+        return await con.fetchval("SELECT * FROM dwr.batch_started() AS bid;")
+
+
+async def batch_succeeded(*, pool: asyncpg.Pool, batch_id: int, execution_millis: int) -> int:
+    async with pool.acquire(timeout=_DEFAULT_TIMEOUT) as con:
+        return await con.execute(
+            "CALL dwr.batch_succeeded (p_batch_id := $1, p_execution_millis := $2);",
+            batch_id, execution_millis,
+        )
+
+
+async def gather_with_limited_concurrency(n: int, *tasks: asyncio.Task) -> tuple[asyncio.Future, ...]:
     semaphore = asyncio.Semaphore(n)
 
     async def wrap_task(task):
@@ -42,3 +82,44 @@ async def get_proc_names_by_pattern(
            """, schema, like
         )
         return {row[0] for row in result}
+
+
+async def proc_failed(
+    *,
+    pool: asyncpg.Pool,
+    proc_id: int,
+    error_message: str,
+    context: dict[str, typing.Hashable],
+) -> None:
+    async with pool.acquire(timeout=_DEFAULT_TIMEOUT) as con:
+        await con.execute(
+            "CALL dwr.proc_failed(p_proc_id := $1, p_error_message := $2, p_context := $3);",
+            proc_id, error_message, context,
+        )
+
+
+async def proc_started(*, pool: asyncpg.Pool, batch_id: int, proc_name: str) -> int:
+    async with pool.acquire(timeout=_DEFAULT_TIMEOUT) as con:
+        return await con.fetchval(
+            "SELECT * FROM dwr.proc_started(p_batch_id := $1, p_name := $2);",
+            batch_id, proc_name,
+        )
+
+
+async def proc_succeeded(*, pool: asyncpg.Pool, proc_id: int, execution_millis: int) -> None:
+    async with pool.acquire(timeout=_DEFAULT_TIMEOUT) as con:
+        await con.execute(
+            "CALL dwr.proc_succeeded(p_proc_id := $1, p_execution_millis := $2);",
+            proc_id, execution_millis,
+        )
+
+
+async def run_proc(
+    *,
+    pool: asyncpg.Pool,
+    schema: str,
+    proc_name: str,
+    incremental: bool,
+) -> None:
+    async with pool.acquire(timeout=10) as con:
+        await con.execute(f"CALL {schema}.{proc_name}(p_incremental := $1);", incremental)
--- src/service/refresh.py
+++ src/service/refresh.py
@@ -1,41 +1,134 @@
+import datetime
+import traceback
+
 import asyncpg
-import loguru as loguru
+from loguru import logger
 
 from src.adapter import db
 
 __all__ = ("refresh",)
 
 
-async def refresh(*, incremental: bool, max_connections: int, schema: str, connection_string: str) -> None:
+async def refresh(
+    *,
+    incremental: bool,
+    max_connections: int,
+    schema: str,
+    connection_string: str,
+) -> None:
+    start_time = datetime.datetime.now()
+
     pool = await asyncpg.create_pool(connection_string)
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_p\_%")
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_h\_%")
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_l\_%")
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_sal\_%")
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_s\_%")
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_%\_dim")
-    await _run_procs_matching_pattern(pool=pool, schema=schema, incremental=incremental, max_connections=max_connections, like="refresh\_%\_fact")
+    batch_id = await db.batch_started(pool=pool)
+    try:
+        for pattern in (
+            r"refresh\_p\_%",
+            r"refresh\_h\_%",
+            r"refresh\_l\_%",
+            r"refresh\_sal\_%",
+            r"refresh\_s\_%",
+            r"refresh\_%\_dim",
+            r"refresh\_%\_fact",
+        ):
+            await _run_procs_matching_pattern(
+                pool=pool,
+                batch_id=batch_id,
+                schema=schema,
+                incremental=incremental,
+                max_connections=max_connections,
+                like=pattern,
+            )
+
+            execution_millis = int((datetime.datetime.now() - start_time).total_seconds() * 1000)
+
+            await db.batch_succeeded(
+                pool=pool,
+                batch_id=batch_id,
+                execution_millis=execution_millis,
+            )
+    except Exception as e:
+        try:
+            await db.batch_failed(
+                pool=pool,
+                batch_id=batch_id,
+                error_message=f"{e!s}\n{traceback.format_exc()}",
+                context={
+                    "incremental": incremental,
+                    "max_connections": max_connections,
+                    "schema": schema,
+                },
+            )
+        except Exception as e:
+            logger.exception(e)
+            raise
 
 
-async def _run_proc(*, pool: asyncpg.Pool, schema: str, stored_proc: str, incremental: bool) -> None:
-    async with pool.acquire(timeout=10) as con:
-        loguru.logger.info(f"Starting {schema}.{stored_proc}...")
-        await con.execute(f"CALL {schema}.{stored_proc}(p_incremental := $1);", incremental)
-        loguru.logger.info(f"{schema}.{stored_proc} completed successfully.")
+async def _run_proc(
+    *,
+    pool: asyncpg.Pool,
+    batch_id: int,
+    schema: str,
+    stored_proc: str,
+    incremental: bool,
+) -> None:
+    proc_id = await db.proc_started(pool=pool, batch_id=batch_id, proc_name=stored_proc)
+    try:
+        start_time = datetime.datetime.now()
+        await db.run_proc(pool=pool, schema=schema, proc_name=stored_proc, incremental=incremental)
+        execution_millis = int((datetime.datetime.now() - start_time).total_seconds() * 1000)
+        await db.proc_succeeded(pool=pool, proc_id=proc_id, execution_millis=execution_millis)
+    except Exception as e:
+        try:
+            await db.proc_failed(
+                pool=pool,
+                proc_id=proc_id,
+                error_message=f"An error occurred while running {schema}.{stored_proc}: {e!s}\n{traceback.format_exc()}",
+                context={
+                    "schema": schema,
+                    "stored_proc": stored_proc,
+                    "incremental": incremental,
+                },
+            )
+        except Exception as e:
+            logger.exception(e)
+            raise
 
 
-async def _run_procs(*, pool: asyncpg.Pool, schema: str, stored_procs: set[str], incremental: bool, max_connections: int) -> None:
+async def _run_procs(
+    *,
+    pool: asyncpg.Pool,
+    batch_id: int,
+    schema: str,
+    stored_procs: set[str],
+    incremental: bool,
+    max_connections: int,
+) -> None:
     tasks = [
-        _run_proc(pool=pool, schema=schema, stored_proc=stored_proc, incremental=incremental)
+        _run_proc(
+            pool=pool,
+            batch_id=batch_id,
+            schema=schema,
+            stored_proc=stored_proc,
+            incremental=incremental,
+        )
         for stored_proc in stored_procs
     ]
     await db.gather_with_limited_concurrency(max_connections, *tasks)
 
 
-async def _run_procs_matching_pattern(*, pool: asyncpg.Pool, schema: str, like: str, incremental: bool, max_connections: int) -> None:
+async def _run_procs_matching_pattern(
+    *,
+    pool: asyncpg.Pool,
+    batch_id: int,
+    schema: str,
+    like: str,
+    incremental: bool,
+    max_connections: int,
+) -> None:
     procs = await db.get_proc_names_by_pattern(pool=pool, schema=schema, like=like)
     await _run_procs(
         pool=pool,
+        batch_id=batch_id,
         schema=schema,
         stored_procs=procs,
         incremental=incremental,
